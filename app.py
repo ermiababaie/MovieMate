b3ae1a6ada238643762c5ee98013918cc9dad48c
ermia babaie <160699260+ermiababaie@users.noreply.github.com>
2026-02-08 23:03:04 +0330

    Update app.py_secretKey

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sqlalchemy import text
 from PIL import Image  
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key_here"
+app.secret_key = os.environ.get("SECRET_KEY")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
